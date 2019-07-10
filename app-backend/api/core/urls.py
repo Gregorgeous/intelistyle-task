f0afb1ea49124285700a4621c1425b7fb969a99f
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-07-10 12:15:40 +0100

    Create the url for endpoints for the model via the 'DefaultRouter'.

--- app-backend/api/core/urls.py
+++ app-backend/api/core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .views import GarmentViewSet
 
+router = DefaultRouter()
+router.register(r'garments', GarmentViewSet)
 
 urlpatterns = [
+    path("", include(router.urls)),
 ]
