52d64c7abb43975db1980e92b4ca395af782041d
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-07-10 12:09:39 +0100

    Create the db model for the garments.

--- app-backend/api/core/models.py
+++ app-backend/api/core/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+class Garment(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=400)
+    picture = models.ImageField()
+    
+    def __str_(self):
+        return f"Garment: {self.name}"
