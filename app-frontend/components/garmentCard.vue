89488c04fdbb5c94d55b947456d70ec065f9e250
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-07-10 12:32:34 +0100

    Create the 'garmentCard' vue component showing an individual garment given the right data format.

---
+++ app-frontend/components/garmentCard.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="garment-card max-w-sm rounded overflow-hidden shadow-lg hover:shadow-xl hover:border-4 ">
+		<img class="w-full garment-card__image"
+			:src="garmentObject.picture"
+			alt="Women in a black coat">
+		<div class="garment-card__name font-bold text-white text-base md:text-xl mb-2 opacity-75">{{garmentObject.name}}</div>
+		<div class="garment-card__description text-gray-200 text-base opacity-75">
+			{{garmentObject.description}}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+  props: ['garmentObject']
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
