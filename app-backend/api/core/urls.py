f09e02fd80b9530bf146dd64d05e6ec2d8f607d0
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-07-10 12:53:55 +0100

    Adding search feature #3: Backend: import and register the new searchListView set to the API router.

--- app-backend/api/core/urls.py
+++ app-backend/api/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import GarmentViewSet
+from .views import GarmentViewSet, searchListView
 
 router = DefaultRouter()
 router.register(r'garments', GarmentViewSet)
+router.register(r'search', searchListView, basename="search")
 
 urlpatterns = [
     path("", include(router.urls)),
