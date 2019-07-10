a7f1182f395f796dc2a4035f32f48e6c8dab6413
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-07-10 12:49:31 +0100

    Adding search feature #1: Backend: Create Garment queryset and manager for complex "search()" ORM call.

--- app-backend/api/core/models.py
+++ app-backend/api/core/models.py
@@ -1,8 +1,32 @@
 from django.db import models
+from django.db.models import Q
+
+class GarmentQuerySet(models.QuerySet):
+    def search(self, query):
+        print("working")
+        lookup = (
+                    Q(name__icontains=query) |
+                    Q(description__icontains=query) 
+                    )
+        print("LOOKUP:")
+        print(lookup)
+        return self.filter(lookup)
+
+class GarmentManager(models.Manager):
+    def get_queryset(self):
+        return GarmentQuerySet(self.model, using=self._db)
+
+    def search(self, query=None):
+        if query is None:
+            return self.get_queryset().all()
+        return self.get_queryset().published().search(query)
+
+
 class Garment(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=400)
     picture = models.ImageField()
     
+    objects = GarmentManager()
     def __str_(self):
         return f"Garment: {self.name}"
