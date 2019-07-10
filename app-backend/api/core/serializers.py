891eed17df08a364ebec2e054b3189b5ed8a61d9
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-07-10 12:10:29 +0100

    Create an API serialiser (Django Rest Framework thing) for the Garment model.

--- app-backend/api/core/serializers.py
+++ app-backend/api/core/serializers.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from .models import Garment 
+
+class GarmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Garment
+        fields = ("id", "name", "description", "picture")
