c0e29db8622a364816578e8ef0db64b324485997
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-07-10 12:14:30 +0100

    Create the viewset for the Garments display endpoint.
    This viewset will handle all the CRUD functionalities given an appropriate enpoint and an HTTP method (this will be further handled by Django router).
    Note that the "UD" operations are set to require an authentication. This can be done by creating a superuser through django manage.py and logging in on the 'admin/' endpoint.

--- app-backend/api/core/views.py
+++ app-backend/api/core/views.py
@@ -1,3 +1,11 @@
 from rest_framework import viewsets
+from .serializers import GarmentSerializer  
+from .models import Garment
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class GarmentViewSet(viewsets.ModelViewSet):
+    serializer_class = GarmentSerializer
+    queryset = Garment.objects.all().order_by('id')
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
